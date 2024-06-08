25fc1efe862ca7e30927618a064d3a5146d0b774
Yorktown <example@mail.com>
2024-06-08 16:20:44 +0700

    добавлен блок с карточками

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <HeaderComponent />
   <SliderComponent />
+  <InfoBlock />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import SliderComponent from "./components/SliderComponent.vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
+import InfoBlock from "./components/InfoBlock.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HeaderComponent,
     SliderComponent,
-
+    InfoBlock,
   },
 });
 </script>
@@ -23,5 +25,6 @@ export default defineComponent({
   width: 1400px;
   margin: 0 auto 0 auto;
   font-family: 'Gilroy', sans-serif;
+  color: #22253b;
 }
 </style>
