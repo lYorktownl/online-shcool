ba240772c78ac9b9ee129cb6151678011e1305d9
Yorktown <example@mail.com>
2024-06-08 11:52:19 +0700

    подготовленная базовая структура файлов, имортирован swiper

---
+++ src/components/SliderComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <swiper :slides-per-view="1" navigation pagination>
+    <swiper-slide>
+      <div>Слайд 1</div>
+    </swiper-slide>
+    <swiper-slide>
+      <div>Слайд 2</div>
+    </swiper-slide>
+    <swiper-slide>
+      <div>Слайд 3</div>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script>
+
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/swiper-bundle.css";
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
